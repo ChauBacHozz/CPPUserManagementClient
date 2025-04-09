eb43c43e6cfb031c47bf90d75a104fb308873180
Nguyen Duc Phong <phongndsaiyan@gmail.com>
2025-04-09 13:25:00 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-#include "User.h"
-#include <string>
+#include <arrow/io/file.h>
+#include <parquet/stream_reader.h>
 
-int main()
-{
-    std::string userName = "Nguyen Duc Phong";
-    std::string userAccount = "Phong123";
-    std::string userPassword = "ndPhongZ2222";
-    int userPoint = 12;
 
-    User tempUser(userName, userAccount, userPassword, userPoint);
-    tempUser.printUserInfo();
+int main () {
+    std::shared_ptr<arrow::io::ReadableFile> infile;
+
+    PARQUET_ASSIGN_OR_THROW(
+        infile,
+        arrow::io::ReadableFile::Open("D:/VS_Workspace/CppTemplate/assets/test.parquet"));
+
+    parquet::StreamReader os{parquet::ParquetFileReader::Open(infile)};
+
+    int64_t size;
+    double price;
+
+    while (!os.eof())
+    {
+        os >> size >> price >> parquet::EndRow;
+        std::cout << "SIZE: " << size << " PRICE: " << price << std::endl;
+    }
+
     return 0;
 }

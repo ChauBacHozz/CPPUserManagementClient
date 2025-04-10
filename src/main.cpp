a726cc0f418c70e386b3154036e4750d19de182b
Nguyen Duc Phong <phongndsaiyan@gmail.com>
2025-04-10 13:59:50 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,19 @@ int main () {
 
     PARQUET_ASSIGN_OR_THROW(
        infile,
-       arrow::io::ReadableFile::Open("../assets/test.parquet"));
+       arrow::io::ReadableFile::Open("../assets/account.parquet"));
  
     parquet::StreamReader stream{parquet::ParquetFileReader::Open(infile)};
  
-    int64_t s;
-    double p;
+    std::string name;
+    std::string age;
+    std::string point;
 
  
     while (!stream.eof() )
     {
-        stream >> s >> p >> parquet::EndRow;
-        std::cout << "SIZE: " << s << " PRICE: " << p << std::endl;
+        stream >> name >> age >> point >> parquet::EndRow;
+        std::cout << "Name: " << name << " Age: " << age << " Point: " << point << std::endl;
        // ...
     }
 
